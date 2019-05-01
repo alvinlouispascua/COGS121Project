0d6f22959253eac2852a7517f1d3bd7b48c4b0ae
Marc Raphael <marc.raphael12@gmail.com>
2019-05-01 10:19:45 -0700

    firebase

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
 
-
 import Home from "./pages/Home";
 import Food from "./pages/Food";
 import Shelter from "./pages/Shelter";
